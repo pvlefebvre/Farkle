279c9427551b599a5d5f3cb1d38f9396ce25df28
Paul Lefebvre <pvl4j@virginia.edu>
2016-06-03 13:33:40 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by Paul Lefebvre on 6/3/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Farkle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Paul Lefebvre on 6/3/16.
+//  Copyright © 2016 Paul Lefebvre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
